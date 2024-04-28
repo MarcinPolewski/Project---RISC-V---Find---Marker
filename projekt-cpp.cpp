3135d223220b58192b0d38b344b504142877093d
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-04-28 11:03:50 +0200

    started experimenting with bitmap

--- projekt-cpp.cpp
+++ projekt-cpp.cpp
@@ -12,11 +12,89 @@
 #define HEADER_SIZE 54          // how many bytes are in theheader
 
 #define N 100000
-#define fileName "example_markers.bmp"
+#define fileName "example_markers_only_5_corner.bmp"
 
-bool isPixelBlack()
+bool isPixelBlack(char *image, int idx)
 {
-    return false;
+    return (image[idx] == 0x0 && image[idx + 1] == 0x0 && image[idx + 2] == 0x0);
+}
+
+int getLineLength(char *image, int start_idx)
+{
+    // first pixel must be black
+    start_idx += 3;
+
+    // calculate how many iterations left in this line
+    int temp = 3 * WIDTH;
+    temp -= start_idx % (3 * WIDTH);
+
+    int counter = 1;
+
+    while (temp != 0 && isPixelBlack(image, start_idx))
+    {
+        counter++;
+        temp--;
+        start_idx += 3;
+    }
+
+    if (temp == 0) // end of line reached
+        return 0;
+    return counter;
+}
+
+bool checkLBlack(char *image, int start_idx, int length)
+{
+    // mozna zalozyc, ze nie wyjdziemy poza linie obraz
+
+    // checking horizontal line
+    int it = start_idx;
+    for (int i = length - 1; i >= 0; --i, it += 3)
+    {
+        if (!isPixelBlack(image, it))
+            return false;
+    }
+
+    // checking if most right pixel is white
+    it += 3;
+    if (!isPixelBlack(image, it))
+        return false;
+
+    // checking vertical line
+    it = start_idx;
+    for (int i = length / 2; i >= 0; --i, it -= WIDTH)
+    {
+        if (!isPixelBlack(image, it))
+            return false;
+    }
+    // chekcking if the most down pixel is white
+    it -= WIDTH;
+    if (!isPixelBlack(image, it))
+        return false;
+
+    return true;
+}
+
+bool checkLWhite(char *image, int start_idx, int length)
+{
+    // mozna zalozyc, ze nie wyjdziemy poza linie obraz
+
+    // checking horizontal line
+    int it = start_idx;
+    for (int i = length - 1; i >= 0; --i, it += 3)
+    {
+        if (isPixelBlack(image, it))
+            return false;
+    }
+
+    // checking vertical line
+    it = start_idx;
+    for (int i = length / 2; i >= 0; --i, it -= WIDTH)
+    {
+        if (isPixelBlack(image, it))
+            return false;
+    }
+
+    return true;
 }
 
 void readBMP()
@@ -57,21 +135,69 @@ void readBMP()
     char image[3 * numberOfPixels];
     reader.read(image, 3 * numberOfPixels);
 
-    // reading pixels
-    char c;
+    // reading pixels from array and printing
+    // iteration from left to right, from top to bottom
+    // for (int i = height - 1; i >= 0; --i)
+    // {
+    //     std::cout << HEIGHT - i << " :";
+    //     for (int j = 0; j < width; ++j)
+    //     {
+    //         int idx = i * width + j;
+    //         idx *= 3; // because there
+    //         if (isPixelBlack(image, idx))
+    //             std::cout << "1";
+    //         else
+    //             std::cout << "0";
+    //     }
+    //     std::cout << '\n';
+    // }
+
     for (int i = height - 1; i >= 0; --i)
     {
         for (int j = 0; j < width; ++j)
         {
             int idx = i * width + j;
-            idx *= 3; // because there
-            // std::cout << *reinterpret_cast<uint32_t *>(&header[idx]) << std::endl;
-            if (image[idx] == 0x0 && image[idx + 1] == 0x0 && image[idx + 2] == 0x0)
-                std::cout << "1";
-            else
-                std::cout << "0";
+            idx *= 3;
+
+            if (isPixelBlack(image, idx)) // black pixel found
+            {
+                int l = getLineLength(image, idx);
+                if (l == 0) // that means that is the last pixel in a row
+                {
+                    continue;
+                }
+                std::cout << l << std::endl;
+                int a = l;
+                if (checkLWhite(image, idx - 3 + (3 * WIDTH), a + 1))
+                {
+                    std::cout << "white L found at: "
+                              << "Column: " << j << " Row: " << HEIGHT - i << std::endl;
+                    while (a != 0 && checkLBlack(image, idx, a))
+                    {
+                        std::cout << "Black L found at: "
+                                  << "Column: " << j << " Row: " << HEIGHT - i << std::endl;
+                        idx += (3 * 1) - (3 * WIDTH);
+                        a -= 1;
+                    }
+
+                    if (a != 0)
+                    {
+                        idx += 3 - (3 * WIDTH);
+                        a -= 1;
+                        if (checkLWhite(image, idx, a))
+                        {
+                            std::cout << "!!white L found at: "
+                                      << "Column: " << j << " Row: " << HEIGHT - i << std::endl;
+                            // std::cout << "found! Column: " << idx % WIDTH << " Row: " << idx / HEIGHT << std::endl;
+                        }
+                    }
+                }
+
+                j--;
+                j += l;
+                // std::cout << std::endl;
+            }
         }
-        std::cout << '\n';
     }
 
     reader.close();
